fc403211ed5dd9e158e44bfa798925b5a34fa6f5
Mekan777-alt <mekan2678@gmail.com>
2023-12-27 17:21:43 +0300

    modify ex1

--- ex1/ex1.py
+++ ex1/ex1.py
@@ -7,35 +7,33 @@ class User:
 
     def add_contact(self, contact, tag):
         if tag not in self.phone_books:
-            print("Данный тэг не найден")
-            return
+            raise ValueError("Данный тэг не найден")
         self.phone_books[tag].add_contact(contact)
 
     def delete_friends(self, firstname):
         if firstname in self.friends:
             self.friends.remove(firstname)
-            print(f"{firstname} удален из списка друзей")
-        print("Такого друга не существует")
+            raise ValueError(f"{firstname} удален из списка друзей")
+        raise ValueError("Такого друга не существует")
 
     def add_friends(self, user):
         if user not in self.friends:
             self.friends.append(user)
             print(f"Пользователь {user.firstname} добавлен в друзья")
         else:
-            print("Вы уже дружите")
+            raise ValueError("Вы уже дружите")
 
     def add_phone_book(self, tag):
         if tag not in self.phone_books:
             self.phone_books[tag] = PhoneBook(tag)
         else:
-            print(f"Данная телефонная книга {tag} уже существует")
+            raise ValueError(f"Данная телефонная книга {tag} уже существует")
 
     def share_phone_books(self, user, tag):
         if user not in self.friends:
-            print(f"C {user} вы не друзья")
-            return
+            raise ValueError(f"C {user} вы не друзья")
         if tag not in self.phone_books:
-            print(f"Телефонная книга с тегом {tag} не существует")
+            raise ValueError(f"Телефонная книга с тегом {tag} не существует")
         user.phone_books[tag] = self.phone_books[tag]
 
 
@@ -46,13 +44,13 @@ class PhoneBook:
 
     def add_contact(self, contact):
         if contact in self.contacts:
-            print("Данный контакт уже существует")
+            raise ValueError("Данный контакт уже существует")
         self.contacts.append(contact)
 
     def delete_contact(self, contact):
         if contact in self.contacts:
             self.contacts.remove(contact)
-        print("Данный контакт не существует")
+        raise ValueError("Данный контакт не существует")
 
     def filter_user(self, user):
         return [contact for contact in self.contacts if contact.user == user]
@@ -76,6 +74,18 @@ class Admin:
     @staticmethod
     def remove_contact_to_phone_book(contact, phone_book):
         phone_book.delete_contact(contact)
+
+    @staticmethod
+    def add_friends(user, friend):
+        user.add_friends(friend)
+
+    @staticmethod
+    def add_phone_book(user, tag):
+        user.add_phone_book(tag)
+
+    @staticmethod
+    def share_phone_books(user1, user2, tag):
+        user1.share_phone_books(user2, tag)
 
 
 
